47984c3caf996a5020b135e6cea226f3086264e4
Aisha Nomani <aishan@usf.edu>
2025-01-23 23:53:21 -0500

    Task 2 - Product Details.

--- coding-challenge-1/script.js
+++ coding-challenge-1/script.js
@@ -1,16 +1,20 @@
 // Task 1: Variable Declaration
+// Scenario: Employee Information
+
+console.log("********** Employee Information **********");
 let employeeName = "Jane Smith"; 
 console.log("Employee Name: " + employeeName + " - Variable Type: " + typeof(employeeName));  
-const employeeID = "111-222-3333";
+const employeeID = 1112223333;
 console.log("Employee ID: " + employeeID + " - Variable Type: " + typeof(employeeID));
 var isActive = true;
 console.log("Is Employee Active: " + isActive + " - Variable Type: " + typeof(isActive));
 
-// Task 2: Arithmetic Operations
-let total = 100 + 50; 
-console.log(total);  
-
-// Task 3: Example Problem  
-let accountBalance = 100000;  
-accountBalance += 50000;  
-console.log(accountBalance);  
+// Task 2: Primitive Data Types
+// Scenario: Product Details
+console.log("********** Product Information **********");
+let productName = "Apple MacBook"; 
+console.log("Product Name: " + productName + " - Variable Type: " + typeof(productName));  
+const productPrice = 1000;
+console.log("Product Price: $" + productPrice + " - Variable Type: " + typeof(employeeID));
+var isAvailable = true;
+console.log("Is Product Available: " + isAvailable + " - Variable Type: " + typeof(isAvailable));
