44a7549c43146905a2a2ad914c353095cb5f4f9f
Aisha Nomani <aishan@usf.edu>
2025-01-23 22:59:28 -0500

    Initial Commit

---
+++ coding-challenge-1/script.js
@@ -0,0 +1,12 @@
+// Task 1: Variable Declaration
+let employeeName = "Employee Name"; 
+console.log(employeeName);  
+
+// Task 2: Arithmetic Operations
+let total = 100 + 50; 
+console.log(total);  
+
+// Task 3: Example Problem  
+let accountBalance = 100000;  
+accountBalance += 50000;  
+console.log(accountBalance);  
