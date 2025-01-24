35a9f31eef4752bd22f40d1a48cd0f44e8d04104
Aisha Nomani <aishan@usf.edu>
2025-01-24 00:06:31 -0500

    Task 3 - Financial Transactions

--- coding-challenge-1/script.js
+++ coding-challenge-1/script.js
@@ -11,10 +11,26 @@ console.log("Is Employee Active: " + isActive + " - Variable Type: " + typeof(is
 
 // Task 2: Primitive Data Types
 // Scenario: Product Details
-console.log("********** Product Information **********");
+console.log("********** Product Details **********");
 let productName = "Apple MacBook"; 
 console.log("Product Name: " + productName + " - Variable Type: " + typeof(productName));  
 const productPrice = 1000;
 console.log("Product Price: $" + productPrice + " - Variable Type: " + typeof(employeeID));
 var isAvailable = true;
 console.log("Is Product Available: " + isAvailable + " - Variable Type: " + typeof(isAvailable));
+
+
+// Task 3: Number Data Type
+// Scenario: Financial Transactions 
+console.log("********** Financial Transactions  **********");
+let accountBalance = 5000; 
+console.log("Initial Account Balance: $" + accountBalance + " - Variable Type: " + typeof(accountBalance));  
+// Perform arithmetic operations
+accountBalance += 1500;  // Deposit
+console.log("After Deposit: $" + accountBalance);
+accountBalance -= 2000;  // Withdrawal
+console.log("After Withdrawal: $" + accountBalance);
+accountBalance *= 1.05;  // Interest (5% increase)
+console.log("After Interest: $" + accountBalance);
+accountBalance /= 2;  // Split balance in half
+console.log("After Split: $" + accountBalance);
